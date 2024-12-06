c58af6fb8bbc1eb4c511caa4b2b660c3f00b5435
heather bickish <hbickish@gmail.com>
2024-12-06 12:06:37 -0700

    displaying posts

---
+++ src/components/Posts.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { Post } from "@/models/Post";
+
+const props = defineProps({
+  postProp: { type: Post, required: true }
+})
+</script>
+
+
+<template>
+  <section class="row justify-content-center">
+    <div class="col-md-7 shadow my-3">
+      <div class="m-5">
+        <img :src="postProp.creator.picture" :alt="postProp.creator.name" class="creator-img">
+        <span class="ms-3">{{ postProp.creator.name }}</span>
+      </div>
+      <p class="ms-5">{{ postProp.createdAt.toLocaleDateString() }}</p>
+      <p class="ms-5">{{ postProp.body }}</p>
+      <div class="d-flex justify-content-center">
+        <img :src="postProp.imgUrl" alt="" class="post-img">
+      </div>
+      <div>
+        <p class="text-end mdi mdi-heart me-5"></p>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+.creator-img {
+  height: 10dvh;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.post-img {
+  height: 30dvh;
+  width: 60dvh;
+  object-fit: cover;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,18 +1,22 @@
 <script setup>
+import { AppState } from "@/AppState";
+import Posts from "@/components/Posts.vue";
 import { postsService } from "@/services/PostsService";
 import { logger } from "@/utils/Logger";
 import Pop from "@/utils/Pop";
-import { onMounted } from "vue";
+import { computed, onMounted } from "vue";
 
-onMounted(()=> 
-getAllPosts()
+onMounted(() =>
+  getAllPosts()
 )
+
+const posts = computed(() => AppState.posts)
 
 async function getAllPosts() {
   try {
     await postsService.getAllPosts()
   }
-  catch (error){
+  catch (error) {
     Pop.meow(error);
     logger.log('getting posts', error)
   }
@@ -20,7 +24,11 @@ async function getAllPosts() {
 </script>
 
 <template>
-  <h1>home page</h1>
+  <div class="container">
+    <div v-for="post in posts" :key="post.id" class="col-12">
+      <Posts :postProp="post" />
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>

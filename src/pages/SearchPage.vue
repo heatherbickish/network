f5c4f039b836904149f9d07672bf759eb72cbc2b
heather bickish <hbickish@gmail.com>
2024-12-07 11:43:54 -0700

    created search form

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -4,7 +4,27 @@
 
 
 <template>
-  <h1>Search page</h1>
+  <div class="container">
+    <section class="row justify-content-center">
+      <div class="col-12">
+        <div class="mt-4">
+          <h1>Search Posts</h1>
+        </div>
+      </div>
+      <div class="col-md-8 shadow rounded">
+        <form>
+          <div class="mb-3">
+            <label for="searchPosts" class="form-label"></label>
+            <div class="d-flex">
+              <input type="text" class="form-control" id="searchPosts" placeholder="Search...">
+              <button class="btn btn-outline-info" type="submit" title="Search for posts"><i
+                  class="mdi mdi-magnify"></i></button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  </div>
 </template>
 
 

01a17c9859bfb6854e53b48723d65cde462cfa41
heather bickish <hbickish@gmail.com>
2024-12-06 19:43:36 -0700

    three dots only appear on my posts

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,9 +1,13 @@
 <script setup>
+import { AppState } from "@/AppState";
 import { Post } from "@/models/Post";
+import { computed } from "vue";
 
 const props = defineProps({
   postProp: { type: Post, required: true }
 })
+
+const account = computed(() => AppState.account)
 </script>
 
 
@@ -11,9 +15,12 @@ const props = defineProps({
   <section class="row justify-content-center">
     <div class="col-md-6 shadow my-3 rounded">
       <div class="m-5">
+        <h4 v-if="account?.id == postProp.creatorId" class="text-end">...</h4>
         <router-link :to="{ name: 'Profile', params: { profileId: postProp.creatorId } }">
           <img :src="postProp.creator.picture" :alt="postProp.creator.name" class="creator-img">
           <span class="ms-3">{{ postProp.creator.name }}</span>
+
+          <h4></h4>
         </router-link>
       </div>
       <p class="ms-5">{{ postProp.createdAt.toLocaleDateString() }}</p>

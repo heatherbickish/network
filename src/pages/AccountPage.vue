20597f70b69fbffd6758a62929d0453a828a7436
heather bickish <hbickish@gmail.com>
2024-12-08 11:30:51 -0700

    added router link to account form

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -11,7 +11,9 @@ const account = computed(() => AppState.account)
   <div class="text-center">
     <div v-if="account">
       <h1>Welcome {{ account.name }}</h1>
-      <img class="rounded" :src="account.picture" alt="" />
+      <router-link :to="{ name: 'Profile', params: { profileId: account.id } }">
+        <img class="rounded" :src="account.picture" alt="" />
+      </router-link>
       <p>{{ account.email }}</p>
     </div>
     <div class="container">
